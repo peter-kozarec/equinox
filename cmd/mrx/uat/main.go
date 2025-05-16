d8e434fe0f8dcb0af85730c192feecd24641924c
Peter Kozarec <peter.kozarec@outlook.sk>
2025-05-16 22:36:57 +0200

    Added starting point for live and uat

--- cmd/mrx/uat/main.go
+++ cmd/mrx/uat/main.go
@@ -1,5 +1,27 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
+	"go.uber.org/zap"
+	"os"
+	"os/signal"
+	"peter-kozarec/equinox/cmd/mrx"
+	"syscall"
+)
 
+func main() {
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		panic(err)
+	}
+	defer func(logger *zap.Logger) {
+		_ = logger.Sync()
+	}(logger)
+
+	logger.Info(fmt.Sprintf("mrx %s", mrx.Version))
+	defer logger.Info("done")
+
+	_, cancel := signal.NotifyContext(context.Background(), os.Interrupt, syscall.SIGTERM, syscall.SIGKILL)
+	defer cancel()
 }

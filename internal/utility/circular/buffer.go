2ff8a35cbde8009f64eca0b23a1edfc627e57425
Peter Kozarec <peter.kozarec@outlook.sk>
2025-06-05 11:15:48 +0200

    Added circular buffer + tests

---
+++ internal/utility/circular/buffer.go
@@ -0,0 +1,45 @@
+package circular
+
+type Buffer[T any] struct {
+	capacity uint
+
+	head uint
+	size uint
+	data []T
+}
+
+func NewBuffer[T any](capacity uint) *Buffer[T] {
+	if capacity == 0 {
+		panic("capacity must > 0")
+	}
+	return &Buffer[T]{
+		capacity: capacity,
+		data:     make([]T, capacity),
+	}
+}
+
+func (b *Buffer[T]) Capacity() uint {
+	return b.capacity
+}
+
+func (b *Buffer[T]) Size() uint {
+	return b.size
+}
+
+func (b *Buffer[T]) Push(value T) {
+	b.data[b.head] = value
+	b.head = (b.head + 1) % b.capacity
+	if b.size < b.capacity {
+		b.size++
+	}
+}
+
+func (b *Buffer[T]) Get(idx uint) T {
+	if idx >= b.size {
+		panic("index out of range")
+	}
+	if b.size == b.capacity {
+		return b.data[(b.head-1-idx+b.size)%b.capacity]
+	}
+	return b.data[b.head-1-idx]
+}
---
+++ internal/utility/circular/buffer_test.go
@@ -0,0 +1,42 @@
+package circular
+
+import "testing"
+
+func TestBuffer_PushGet(t *testing.T) {
+	b := NewBuffer[int](5)
+	b.Push(0)
+	b.Push(1)
+	b.Push(2)
+	b.Push(3)
+	b.Push(4)
+	b.Push(5)
+	b.Push(6)
+	b.Push(7)
+	b.Push(8)
+
+	c := NewBuffer[int](8)
+	c.Push(0)
+	c.Push(1)
+
+	tests := []struct {
+		name     string
+		result   int
+		expected int
+	}{
+		{"b.Get(0) == 8", b.Get(0), 8},
+		{"b.Get(1) == 7", b.Get(1), 7},
+		{"b.Get(2) == 6", b.Get(2), 6},
+		{"b.Get(3) == 5", b.Get(3), 5},
+		{"b.Get(4) == 4", b.Get(4), 4},
+		{"c.Get(0) == 1", c.Get(0), 1},
+		{"c.Get(1) == 0", c.Get(1), 0},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if tt.result != tt.expected {
+				t.Errorf("got %d, want %d", tt.result, tt.expected)
+			}
+		})
+	}
+}

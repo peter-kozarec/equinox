dd0669a1625dd80a70c0a18c6d0caea8f16f5cf5
Peter Kozarec <peter.kozarec@outlook.sk>
2025-06-15 12:02:31 +0200

    added: test

---
+++ test/backtest/advisor.go
@@ -0,0 +1,102 @@
+package main
+
+import (
+	"github.com/peter-kozarec/equinox/pkg/bus"
+	"github.com/peter-kozarec/equinox/pkg/model"
+	"github.com/peter-kozarec/equinox/pkg/utility/circular"
+	"github.com/peter-kozarec/equinox/pkg/utility/fixed"
+	"github.com/peter-kozarec/equinox/pkg/utility/math"
+	"go.uber.org/zap"
+	"time"
+)
+
+var (
+	Three         = fixed.New(3, 0)
+	NegativeThree = fixed.New(-3, 0)
+)
+
+type Advisor struct {
+	logger *zap.Logger
+	router *bus.Router
+
+	lastTick model.Tick
+	closes   *circular.Buffer[fixed.Point]
+	zScores  *circular.Buffer[fixed.Point]
+
+	posOpen bool
+}
+
+func NewAdvisor(logger *zap.Logger, router *bus.Router) *Advisor {
+	return &Advisor{
+		logger:  logger,
+		router:  router,
+		closes:  circular.NewBuffer[fixed.Point](60),
+		zScores: circular.NewBuffer[fixed.Point](60),
+		posOpen: false,
+	}
+}
+
+func (a *Advisor) NewTick(t model.Tick) {
+	a.lastTick = t
+}
+
+func (a *Advisor) NewBar(b model.Bar) {
+
+	a.closes.Push(b.Close)
+
+	if !a.closes.IsFull() {
+		return
+	}
+
+	closes := a.closes.Data()
+	mean := math.Mean(closes)
+	stdDev := math.StandardDeviation(closes, mean)
+	z := b.Close.Sub(mean).Div(stdDev)
+
+	a.zScores.Push(z)
+
+	if !a.zScores.IsFull() {
+		return
+	}
+
+	if !a.posOpen && a.canTrade() {
+		if z.Gte(Three) {
+			_ = a.router.Post(bus.OrderEvent, model.Order{
+				Command:    model.CmdOpen,
+				OrderType:  model.Market,
+				Size:       fixed.New(1, 2).Neg(),
+				StopLoss:   b.Close.Add(b.Close.Sub(mean)),
+				TakeProfit: mean,
+			})
+			a.posOpen = true
+		} else if z.Lte(NegativeThree) {
+			_ = a.router.Post(bus.OrderEvent, model.Order{
+				Command:    model.CmdOpen,
+				OrderType:  model.Market,
+				Size:       fixed.New(1, 2),
+				StopLoss:   b.Close.Sub(mean.Sub(b.Close)),
+				TakeProfit: mean,
+			})
+			a.posOpen = true
+		}
+	}
+}
+
+func (a *Advisor) PositionClosed(p model.Position) {
+	a.posOpen = false
+}
+
+func (a *Advisor) canTrade() bool {
+
+	if a.lastTick.TimeStamp == 0 {
+		return false
+	}
+
+	t := time.Unix(0, a.lastTick.TimeStamp)
+
+	if t.Hour() < 9 || t.Hour() > 18 {
+		return false
+	}
+
+	return true
+}
---
+++ test/backtest/main.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"github.com/peter-kozarec/equinox/pkg/bus"
+	"github.com/peter-kozarec/equinox/pkg/data/mapper"
+	"github.com/peter-kozarec/equinox/pkg/middleware"
+	"github.com/peter-kozarec/equinox/pkg/simulation"
+	"github.com/peter-kozarec/equinox/pkg/utility/fixed"
+	"go.uber.org/zap"
+	"log"
+	"os"
+	"os/signal"
+	"time"
+)
+
+const (
+	TickBinDir = "C:\\Users\\peter\\market_data\\"
+
+	StartTime = "2019-01-01 00:00:00"
+	EndTime   = "2020-01-01 00:00:00"
+)
+
+func main() {
+	logger, err := zap.NewDevelopment()
+	if err != nil {
+		log.Fatalf("unable to initializing logger: %v", err)
+	}
+	defer logger.Sync()
+
+	router := bus.NewRouter(logger, 1000)
+
+	mp := mapper.NewReader[mapper.BinaryTick](TickBinDir + "eurusd.bin")
+	if err := mp.Open(); err != nil {
+		logger.Fatal("unable to open mapper", zap.Error(err))
+	}
+	defer mp.Close()
+
+	startTime, _ := time.Parse(time.DateTime, StartTime)
+	endTime, _ := time.Parse(time.DateTime, EndTime)
+
+	simConf := simulation.Configuration{
+		BarPeriod:        time.Minute,
+		PipSize:          fixed.New(1, 4),
+		ContractSize:     fixed.New(100000, 0),
+		CommissionPerLot: fixed.New(3, 0),
+		StartBalance:     fixed.New(10000, 0),
+		PipSlippage:      fixed.New(10, 5),
+	}
+
+	audit := simulation.NewAudit(logger, time.Minute)
+	sim := simulation.NewSimulator(logger, router, audit, simConf)
+	exec := simulation.NewExecutor(logger, sim, mp, startTime, endTime)
+
+	telemetry := middleware.NewTelemetry(logger)
+	monitor := middleware.NewMonitor(logger, middleware.MonitorPositionsClosed)
+
+	advisor := NewAdvisor(logger, router)
+	router.TickHandler = middleware.Chain(telemetry.WithTick, monitor.WithTick)(advisor.NewTick)
+	router.BarHandler = middleware.Chain(telemetry.WithBar, monitor.WithBar)(advisor.NewBar)
+	router.OrderHandler = middleware.Chain(telemetry.WithOrder, monitor.WithOrder)(sim.OnOrder)
+	router.PositionOpenedHandler = middleware.Chain(telemetry.WithPositionOpened, monitor.WithPositionOpened)(middleware.NoopPosOpnHdl)
+	router.PositionClosedHandler = middleware.Chain(telemetry.WithPositionClosed, monitor.WithPositionClosed)(advisor.PositionClosed)
+	router.PositionPnLUpdatedHandler = middleware.Chain(telemetry.WithPositionPnLUpdated, monitor.WithPositionPnLUpdated)(middleware.NoopPosUpdHdl)
+	router.EquityHandler = middleware.Chain(telemetry.WithEquity, monitor.WithEquity)(middleware.NoopEquityHdl)
+	router.BalanceHandler = middleware.Chain(telemetry.WithBalance, monitor.WithBalance)(middleware.NoopBalanceHdl)
+
+	if err := exec.LookupStartIndex(); err != nil {
+		logger.Fatal("unable to lookup start index", zap.Error(err))
+	}
+
+	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt, os.Kill)
+	defer cancel()
+
+	go router.ExecLoop(ctx, exec.Feed)
+
+	defer router.PrintStatistics()
+	defer telemetry.PrintStatistics()
+	defer sim.PrintDetails()
+
+	if err := <-router.Done(); err != nil {
+		if !errors.Is(err, context.Canceled) && !errors.Is(err, mapper.EOF) {
+			logger.Fatal("unexpected error during execution", zap.Error(err))
+		}
+	}
+
+	sim.CloseAllOpenPositions()
+
+	report := audit.GenerateReport()
+	report.Print(logger)
+}

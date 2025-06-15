a51c81d3aa1aa425101f25508948bd5e79a65bc1
Peter Kozarec <peter.kozarec@outlook.sk>
2025-06-15 10:29:59 +0200

    added: middleware noop handlers

---
+++ pkg/middleware/noop.go
@@ -0,0 +1,17 @@
+package middleware
+
+import (
+	"github.com/peter-kozarec/equinox/pkg/model"
+	"github.com/peter-kozarec/equinox/pkg/utility/fixed"
+)
+
+var (
+	NoopTickHdl    = func(model.Tick) {}
+	NoopBarHdl     = func(model.Bar) {}
+	NoopEquityHdl  = func(fixed.Point) {}
+	NoopBalanceHdl = func(fixed.Point) {}
+	NoopPosOpnHdl  = func(model.Position) {}
+	NoopPosUpdHdl  = func(model.Position) {}
+	NoopPosClsHdl  = func(model.Position) {}
+	NoopOrderHdl   = func(model.Order) {}
+)

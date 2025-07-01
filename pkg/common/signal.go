4299948c5dba46b91200681201cfc0d0dbde999d
Peter Kozarec <peter.kozarec@fiserv.com>
2025-07-01 14:25:15 +0200

    updated: signal definition

--- pkg/common/signal.go
+++ pkg/common/signal.go
@@ -1,20 +1,26 @@
 package common
 
 import (
+	"time"
+
 	"github.com/peter-kozarec/equinox/pkg/utility/fixed"
 	"go.uber.org/zap"
 )
 
 type Signal struct {
-	Target   fixed.Point
-	Strength uint8
-	Reason   string
+	Source    string        // Signal source identifier
+	Target    fixed.Point   // Target price
+	Strength  uint8         // Signal strength (0-100)
+	TimeFrame time.Duration // Bar timeframe (0 for tick)
+	Comment   string        // Aditional comment about the signal
 }
 
 func (s Signal) Fields() []zap.Field {
 	return []zap.Field{
+		zap.String("source", s.Source),
 		zap.String("target", s.Target.String()),
 		zap.Uint8("strength", s.Strength),
-		zap.String("reason", s.Reason),
+		zap.Duration("timeframe", s.TimeFrame),
+		zap.String("comment", s.Comment),
 	}
 }

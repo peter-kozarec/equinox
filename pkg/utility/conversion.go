a304bea5507b5d2d21776a5905e1ceb8a86446cd
Peter Kozarec <peter.kozarec@outlook.sk>
2025-07-05 20:15:32 +0200

    added comment to prevent gosec from reporting integer overflow in conversion.go

--- pkg/utility/conversion.go
+++ pkg/utility/conversion.go
@@ -6,14 +6,14 @@ import (
 
 func U64ToI64(i uint64) (int64, error) {
 	if i&(1<<63) == 0 {
-		return int64(i), nil
+		return int64(i), nil // #nosec G115
 	}
 	return 0, errors.New("integer overflow")
 }
 
 func U64ToI64Unsafe(i uint64) int64 {
 	if i&(1<<63) == 0 {
-		return int64(i)
+		return int64(i) // #nosec G115
 	}
 	panic("integer overflow")
 }
